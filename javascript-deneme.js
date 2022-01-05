08ecdd050400ffa07f5ffc82d74ad6ca29ac7157
piupiunn <96390657+piupiunn@users.noreply.github.com>
2022-01-05 23:25:13 +0300

    deneme

--- javascript-deneme.js
+++ javascript-deneme.js
@@ -1,4 +1,10 @@
-function getCharCount(str) {
-  return str.lenght;
-  ///////////////////////////////
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+
+function getDescription(text) {
+  return text.substring(0, 10);
 }
+
+input.addEventListener("input", (event) => {
+  output.textContent = getDescription(event.currentTarget.value);
+});
