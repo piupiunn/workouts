733bb76b0947d8f92f5c230273bd89df848e9cf9
piupiunn <96390657+piupiunn@users.noreply.github.com>
2022-01-18 12:09:20 +0300

    dsf

--- blog-yazisi.js
+++
@@ -1,1 +0,0 @@
-var bootstrap = require("bootstrap");
--- javascript-deneme.js
+++ javascript-deneme.js
@@ -1,3 +1,10 @@
+import { debounce } from "lodash-es"
+const button = document.querySelector("#my-button");
+
+button.addEventListener("click", debounce(() => {
+    console.log("Button clicked");
+}, 150));
+
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 
