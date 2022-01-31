8dfebe069a1890a9b0b01e6780ca5ab117d6e4a1
piupiunn <96390657+piupiunn@users.noreply.github.com>
2022-01-31 11:00:11 +0300

    dfg

--- bootstrap-todo/react-works/intro/src/App.js
+++ bootstrap-todo/react-works/intro/src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
+
 import CategoryList from "./CategoryList";
 import Navigasyon from "./Navigasyon";
 import ProductList from "./ProductList";
+import { Col, Container, Row } from "reactstrap";
 
 function App() {
+  let categoryInfo = { title: "Category List" };
+  let productInfo = { title: "Product List" };
   return (
     <div>
-      <Navigasyon></Navigasyon>
-      <CategoryList></CategoryList>
-      <ProductList></ProductList>
+      <Container>
+        <Row>
+          <Navigasyon></Navigasyon>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo}></CategoryList>
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo}></ProductList>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- bootstrap-todo/react-works/intro/src/ProductList.js
+++ bootstrap-todo/react-works/intro/src/ProductList.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class ProductList extends Component {
   render() {
     return (
       <div>
-        <h3>Product List</h3>
+        <h3>{this.props.info.title}</h3>
       </div>
     );
   }
